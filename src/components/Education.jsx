b6768c9ce168029e8e2d641fd28602fbd1679a48
Wiellona <wiellonadarlene@gmail.com>
2023-11-22 17:40:56 +0700

    fixing padding

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -2,28 +2,26 @@ import React from 'react'
 
 function Education () {
   return (
-    <div>
-        <section id='education' className='pt-20 pb-6 md:items-center lg:ml-20'>
-            <div className='container lg:mx-40 lg:px-10 lg:text-xl'>
-                <div className='flex flex-wrap'>
-                    <div className="w-full px-4 mb-10 lg:w-1/2 max-w-md md:w-7/12">
-                        <h4 className='font-semibold text-4xl mb-3'>Education</h4>
-                        <h2>SMAN Negeri 2 Kota Tangerang Selatan</h2>
-                        <h3 className='text-slate-400'>2018 - 2021</h3>
-                        <p>Address: Jl. Raya Puspitek, Muncul, Kec. Setu, Kota Tangerang Selatan, Banten 15314</p>
-                        <a href='http://www.sman2tangsel.sch.id/' target='_blank' className='text-blue-500'>sman2tangsel.sch.id</a>
-                    </div>
+    <section id='education' className='pt-36 pb-32'>
+        <h1 className='font-semibold text-4xl mb-10 pb-5 text-center'>Education</h1>
+        <div className='flex flex-row mb-5 mx-40 text-center'>
 
-                    <div className='w-full px-4 mb-10 lg:w-1/2 max-w-md'>
-                    <h2 className='lg:pt-12'>University of Indonesia</h2>
-                    <h3 className='text-slate-400'>2023 - Present</h3>
-                        <p>Address: Jl. Lingkar, Pondok Cina, Kecamatan Beji, Kota Depok, Jawa Barat 16424</p>
-                        <a href='https://ui.ac.id/' target='_blank' className='text-blue-500'>ui.ac.id</a>
-                    </div>
+            <div className='layout flex flex-col-reverse items-center md:flex-row md:justify-start'>
+                <div className='w-full px-4 mb-10 lg:w-1/2'>
+                    <h2 className='font-bold text-xl mb-2 max-w-xl'>University of Indonesia</h2>
+                    <p className='font-medium text-base text-slate-800 max-wl-xl'>Address: Jl. Lingkar, Pondok Cina, Kecamatan Beji, Kota Depok, Jawa Barat 16424</p>
+                    <a href='https://ui.ac.id/' target='_blank' className='text-blue-500'>ui.ac.id</a>
                 </div>
+
+                <div className='w-full px-4 mb-10 lg:w-1/2'>
+                    <h2 className='font-bold text-xl mb-2 max-w-xl'>SMAN 2 Kota Tangerang Selatan</h2>
+                    <p className='font-medium text-base text-slate-800 max-wl-xl'>Address: Jl. Raya Puspitek, Muncul, Kec. Setu, Kota Tangerang Selatan, Banten 15314</p>
+                    <a href='http://www.sman2tangsel.sch.id/' target='_blank' className='text-blue-500'>sman2tangsel.sch.id</a>
+                </div>
+
             </div>
-        </section>
-    </div>
+        </div>
+    </section>
   )
 }
 
