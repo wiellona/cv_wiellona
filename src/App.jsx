f1feca448031d6ab5baaadcde4a677532500f1b3
Wiellona <wiellonadarlene@gmail.com>
2023-11-24 15:50:08 +0700

    remove contact me section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
 import React from 'react'
-import Contact from './components/Contact'
 import Education from './components/Education'
 import Footer from './components/Footer'
 import Intro from './components/Intro'
@@ -8,18 +7,39 @@ import Timeline from './components/Timeline'
 import Projects from './components/Projects'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
+  document.getElementById("index.html");
   return (
     <div>
       <Intro />
       <Education />
       <Timeline />
       <Projects />
-      <Contact />
       <Footer />
     </div>
   );
-}
+
+};
+
+//navbar fixed
+window.onscroll = function () {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+
+  if (window.pageYOffset > fixedNav) {
+    header.classList.add("navbar-fixed");
+  } else {
+    header.classList.remove("navbar-fixed");
+  }
+};
+
+//hamburger
+const hamburger = document.querySelector("#hamburger");
+const navMenu = document.querySelector("#nav-menu");
+
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("hamburger-active");
+  navMenu.classList.toggle("hidden");
+});
 
 export default App
